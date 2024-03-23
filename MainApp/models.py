b427d5c7be5bb4867c69de5080851ecc81bb1eb1
Evgeny Kislov <e.kislow32@yandex.ru>
2024-03-23 14:05:42 +0300

    Добавлен order_by

--- MainApp/models.py
+++ MainApp/models.py
@@ -10,8 +10,15 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ["name", "lang"]
+
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
+    public = models.BooleanField(default=True)  # True = public, False = private
+
+    def __repr__(self) -> str:
+        return f'Snippet({self.name}, {self.lang})'
--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 from MainApp.models import Snippet
 from django.http import Http404, HttpResponseNotFound
-from MainApp.forms import SnippetForm
+from MainApp.forms import SnippetForm, UserRegistrationForm
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
@@ -11,7 +11,18 @@ def index_page(request):
     context = {"pagename": "PythonBin"}
     return render(request, "pages/index.html", context)
 
-@login_required(login_url='home')
+
+@login_required
+def my_snippets(request):
+    snippets = Snippet.objects.filter(user=request.user)
+    context = {
+        "pagename": "Мои сниппеты",
+        "snippets": snippets,
+    }
+    return render(request, "pages/view_snippets.html", context)
+
+
+@login_required(login_url="home")
 def add_snippet_page(request):
     # Создаем пустую форму при запросе методом GET
     if request.method == "GET":
@@ -35,7 +46,7 @@ def add_snippet_page(request):
 
 
 def snippets_page(request):
-    snippets = Snippet.objects.all()
+    snippets = Snippet.objects.filter(public=True)
     context = {
         "pagename": "Просмотр сниппетов",
         "snippets": snippets,
@@ -57,6 +68,7 @@ def snippet_detail(request, snippet_id):
     return render(request, "pages/snippet_detail.html", context)
 
 
+@login_required
 def snippet_edit(request, snippet_id):
     try:
         snippet = Snippet.objects.get(id=snippet_id)
@@ -85,20 +97,40 @@ def snippet_edit(request, snippet_id):
         data_form = request.POST
         snippet.name = data_form["name"]
         snippet.code = data_form["code"]
-        if (change_date := data_form.get("creation_date")):
+        if change_date := data_form.get("creation_date"):
             snippet.creation_date = change_date
+        snippet.public = data_form.get("public", False)
         snippet.save()
         return redirect("snippets-list")
 
 
+@login_required
 def snippet_delete(request, snippet_id):
     snippet = Snippet.objects.get(id=snippet_id)
     snippet.delete()
     return redirect("snippets-list")
 
 
+def create_user(request):
+    context = {"pagename": "Регистрация пользователя"}
+    # Пустая форма для заполнения данных
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context["form"] = form
+        return render(request, "pages/registration.html", context)
+    
+    # Используем данные из формы
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        context["form"] = form
+        return render(request, "pages/registration.html", context)
+
+
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
         # print("username =", username)
@@ -112,10 +144,9 @@ def login(request):
                 "errors": ["wrong username or password"],
             }
             return render(request, "pages/index.html", context)
-    return redirect('home')
+    return redirect("home")
 
 
 def logout(request):
     auth.logout(request)
-    return redirect('home')
-
+    return redirect("home")
